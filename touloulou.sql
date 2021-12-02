e6efc1b9d6c31c77179bc299266065fd1d20f6c2
LMika <lemika.pro@gmail.com>
2021-12-02 09:16:44 +0100

    Database creation script

---
+++ touloulou.sql
@@ -0,0 +1,15 @@
+create database touloulou;
+use touloulou;
+create table employee (
+	id int(11) not null auto_increment,
+    firstname varchar(128) not null,
+    lastname varchar(128) not null,
+    email varchar(45) not null,
+    age int(3) not null,
+    roleTitle varchar(45) not null,
+    phoneNumber varchar(10) not null,
+    address varchar(128) not null,
+    primary key (id),
+    unique key id_unique (id),
+    unique key email_unique (email)
+) engine InnoDB;
